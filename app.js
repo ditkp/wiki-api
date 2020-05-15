09c8ff3e361028aac0049e8c6052b70cceab8cf3
Patryk Garlinski <patrykgarlinski@MacBook-Air.domain>
2020-05-15 15:40:38 -0600

    add db connections

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true,
+useUnifiedTopology: true});
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -14,7 +17,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-//TODO
+const articleSchema = new mongoose.Schema ({
+  title: String,
+  content: String
+})
+const Article = mongoose.model("Article",articleSchema);
+// const article = new Article ({
+// title: "Goku Says",
+// content: "aaaaaaahhhhhhhhhhhhhh" });
+// fruit.save();
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
